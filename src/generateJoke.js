c9c636e445e5d8364fddf92b5fefc72acb7c2731
HunterXNB <106449397+HunterXNB@users.noreply.github.com>
2023-02-24 17:08:42 +0200

    Add files via upload

---
+++ src/generateJoke.js
@@ -0,0 +1,13 @@
+import axios from "axios"
+function generateJoke() {
+    const config = {
+        headers: {
+            accept: 'application/json'
+        }
+    }
+
+    axios.get('https:/icanhazdadjoke.com', config).then(res => {
+        document.getElementById("joke").innerHTML = res.data.joke
+    })
+}
+export default generateJoke
